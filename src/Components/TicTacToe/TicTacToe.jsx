159d32c1561c2271847503c7ccf33debf7d72e61
Ben Frame <brewerytattooer@gmail.com>
2024-06-11 13:51:51 -0400

    icon assets added

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
+
+const TicTacToe = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+export default TicTacToe;
