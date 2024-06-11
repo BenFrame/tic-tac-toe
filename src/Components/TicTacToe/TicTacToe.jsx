7cf6530446d8c884c4f80a51f01b032848b1acd2
Ben Frame <brewerytattooer@gmail.com>
2024-06-11 14:01:38 -0400

    title css styles added

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -5,8 +5,12 @@ import cross_icon from '../Assets/cross.png'
 
 const TicTacToe = () =>{
     return(
-        <div>
+        <div className="container">
+            <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
+            <div className="board">
 
+            </div>
+            <button className="reset">Reset</button>
         </div>
     )
 };
